b5ea0a46dcd748d6474e1f9c49f67eaf243508c8
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-10 18:46:38 -0500

    key press var

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ var l_hand = document.getElementById("lh");
 var r_hand = document.getElementById("rh");
 var peg = document.getElementById("peg");
 var logas = document.getElementById("logas");
+
+
+window.addEventListener("keydown", kd);
+window.addEventListener("keyup", ku);
