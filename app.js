3eb3f4a6fbc4eb6d275b9b72081dd079d40ea965
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-10 19:14:51 -0500

    var list

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ var speedX = 10,
   speedY = 10,
   px = 40,
   py = 40;
+ry = 0;
+ly = 0;
+var bUP = 0,
+  bDN = 0;
+rDir = 0;
+var bW = 0,
+  bS = 0;
+lDir = 0;
+var sL = 0,
+  sR = 0;
+
 
 
 window.addEventListener("keydown", kd);
