5720eb45323ac69d3a3803a994e7521df15b965f
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-10 19:12:28 -0500

    paddle speed

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var fps = 60;
 var playing = false;
 var delay = 0;
 
+var speedX = 10,
+  speedY = 10,
+  px = 40,
+  py = 40;
+
 
 window.addEventListener("keydown", kd);
 window.addEventListener("keyup", ku);
