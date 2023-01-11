34a0110525e3c31b5635e273c33035bb76c2b20b
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-01-10 19:11:36 -0500

    frames per set

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var r_hand = document.getElementById("rh");
 var peg = document.getElementById("peg");
 var logas = document.getElementById("logas");
 
+var fps = 60;
+var playing = false;
+var delay = 0;
+
 
 window.addEventListener("keydown", kd);
 window.addEventListener("keyup", ku);
